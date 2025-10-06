7d9155245d99ce08a27a67cc5c042d1f8380b00d
Alex <azach@student.agh.edu.pl>
2025-10-06 19:30:38 +0200

    dodanie plikow

---
+++ Lab1/Choinka.java
@@ -0,0 +1,12 @@
+public class Choinka{
+    public static void main(String[] args){
+        int x =Integer.parseInt(args[0]);
+        for(int i =1; i <=x; i++){
+            for(int j = 1; j <=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+    }
+}
