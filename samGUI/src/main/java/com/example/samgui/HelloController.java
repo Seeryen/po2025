2a53aac200b48ffff6a8df3da301659e698b9453
Aleksander Zach <aleksanderzach123@gmail.com>
2025-11-24 19:55:16 +0100

    Laby :)

--- samGUI/src/main/java/com/example/samgui/HelloController.java
+++ samGUI/src/main/java/com/example/samgui/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.samgui;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
@@ -10,5 +11,9 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void onStart(ActionEvent actionEvent) {
+        System.out.println("df");
     }
 }
